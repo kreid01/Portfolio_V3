8407fc8b072c17954ff6e5db3752d47c72ca3ab5
Kieran Reid <kieranreid2612@gmail.com>
2025-10-30 19:30:54 +0000

    some responsivness

---
+++ src/pages/blogs.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { BlogMarkdown } from "../generator/BlogMarkdown";
+
+const Blogs: React.FC = () => {
+    const blogs = useQuery(api.blogs.get) ?? [];
+
+    return (
+        <div className="w-[100vw] h-[100vh]  bg-white dark:bg-zinc-900 flex flex-col">
+            <Navigation />
+            <div className="px-12 w-[100%] md:px-32 2xl:w-[50%] mx-auto">
+                {blogs.map((blog) => (
+                    <BlogMarkdown
+                        key={blog._id}
+                        title={blog.title}
+                        description={blog.description}
+                        created={blog.created}
+                        content={blog.contents}
+                        _id={blog._id}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Blogs;
