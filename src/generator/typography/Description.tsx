1d904b9d0123c9fa5094530bcfa94c88a7cfed25
Kieran Reid <kieranreid2612@gmail.com>
2025-11-03 14:07:01 +0000

    Description

---
+++ src/generator/typography/Description.tsx
@@ -0,0 +1,31 @@
+
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+interface DescriptionProps {
+    description: string;
+    isEditing: boolean;
+    onDescriptionChange: (newDescription: string) => void;
+}
+
+export const Description: React.FC<DescriptionProps> = ({ description, isEditing, onDescriptionChange }) => {
+    const [value, setValue] = useState(description);
+    const onChange = (newDescription: string) => {
+        setValue(newDescription);
+        onDescriptionChange(newDescription);
+    }
+
+    return (
+        isEditing ? (
+            <Input
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+                className="w-full text-center !text-lg font-extrabold tracking-tight text-balance p-2 border rounded"
+            />
+        ) : (
+            <p className="scroll-m-20 text-center text-lg font-extrabold tracking-tight text-balance">
+                {value}
+            </p>
+        )
+    )
+}
